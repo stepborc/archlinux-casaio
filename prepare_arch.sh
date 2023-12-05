3e17cf16bf14933f64515e0ec9245a90a1293a70
stepborc <sbgithub@gmail.com>
2023-12-05 10:06:22 +0100

    Update prepare_arch.sh

--- prepare_arch.sh
+++ prepare_arch.sh
@@ -1,1 +1,8 @@
-#
+git clone https://aur.archlinux.org/yay-bin
+cd yay-bin
+makepkg -si
+yay -Syu
+yay -S wget curl smartmontools parted ntfs-3g net-tools udevil samba cifs-utils mergerfs unzip apparmor docker docker-compose rclone
+sudo systemctl enable --now docker
+Sudo bash install.sh
+
